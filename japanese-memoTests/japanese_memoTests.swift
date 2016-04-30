e550b549eec232ede923b36acc61d4299ed08644
Tha Leang <vleango@gmail.com>
2016-04-30 17:28:00 +0900

    initial commit

---
+++ japanese-memoTests/japanese_memoTests.swift
@@ -0,0 +1,36 @@
+//
+//  japanese_memoTests.swift
+//  japanese-memoTests
+//
+//  Created by Tha Leang on 4/30/16.
+//  Copyright © 2016 Tha Leang. All rights reserved.
+//
+
+import XCTest
+@testable import japanese_memo
+
+class japanese_memoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
